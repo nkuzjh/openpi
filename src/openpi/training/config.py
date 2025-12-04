881e6bae34ab522525410b83ce6e61ac62635d4e
zhh <nkuzjh@gmail.com>
2025-12-05 01:12:04 +0800

    @21: config to fst running~

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1037,7 +1037,7 @@ _CONFIGS = [
             base_config=DataConfig(prompt_from_task=True),
             extra_delta_transform=False,
         ),
-        batch_size=4,
+        batch_size=8,
         lr_schedule=_optimizer.CosineDecaySchedule(
             warmup_steps=10_000,
             peak_lr=5e-5,
@@ -1047,7 +1047,8 @@ _CONFIGS = [
         optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
         ema_decay=0.999,
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
-        pytorch_weight_path="/home/jiahao/.cache/openpi/openpi-assets/checkpoints/pi05_base",#"/path/to/your/pytorch_weight_path",
+        pytorch_weight_path="/home/user/yc57963/.cache/openpi/openpi-assets/checkpoints/pi05_base",#"/path/to/your/pytorch_weight_path",
+        # pytorch_weight_path="/home/jiahao/.cache/openpi/openpi-assets/checkpoints/pi05_base",#"/path/to/your/pytorch_weight_path",
         num_train_steps=30_000,
         save_interval=1000,
         wandb_enabled=False,
