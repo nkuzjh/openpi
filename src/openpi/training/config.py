f001cafe8438daee56409f5f1f894d06268888da
zhh <nkuzjh@gmail.com>
2025-12-05 01:15:27 +0800

    @21: config to fst running~

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1037,7 +1037,7 @@ _CONFIGS = [
             base_config=DataConfig(prompt_from_task=True),
             extra_delta_transform=False,
         ),
-        batch_size=8,
+        batch_size=16,
         lr_schedule=_optimizer.CosineDecaySchedule(
             warmup_steps=10_000,
             peak_lr=5e-5,
@@ -1050,7 +1050,7 @@ _CONFIGS = [
         pytorch_weight_path="/home/user/yc57963/.cache/openpi/openpi-assets/checkpoints/pi05_base",#"/path/to/your/pytorch_weight_path",
         # pytorch_weight_path="/home/jiahao/.cache/openpi/openpi-assets/checkpoints/pi05_base",#"/path/to/your/pytorch_weight_path",
         num_train_steps=30_000,
-        save_interval=1000,
+        save_interval=10_000,
         wandb_enabled=False,
     ),
     #
